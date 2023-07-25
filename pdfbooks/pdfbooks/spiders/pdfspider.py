69ae50a58b7434013c030c0ed70b9b531aa89b16
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-07-26 02:22:20 +0300

    spider creation

---
+++ pdfbooks/pdfbooks/spiders/pdfspider.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class PdfspiderSpider(scrapy.Spider):
+    name = "pdfspider"
+    allowed_domains = ["www.pdfdrive.com"]
+    start_urls = ["https://www.pdfdrive.com/"]
+
+    def parse(self, response):
+        pass
