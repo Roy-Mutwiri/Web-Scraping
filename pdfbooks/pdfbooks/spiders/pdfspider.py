daf0da0a06521981aef68a3c0e81cad394bf42a1
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-07-26 03:43:32 +0300

    15 books

--- pdfbooks/pdfbooks/spiders/pdfspider.py
+++ pdfbooks/pdfbooks/spiders/pdfspider.py
@@ -11,10 +11,9 @@ class PdfspiderSpider(scrapy.Spider):
 
         for book in books:
             yield {
-                "Title" : book.css('a h2::text').get(),
+                "Title" : book.css('.file-right a h2::text').get(),
                 "Pages" : book.css('.file-info .fi-pagecount::text').get(),
                 "Year" : book.css('.file-info .fi-year::text').get()
-
 
 
             }
